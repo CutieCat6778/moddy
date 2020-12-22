935a1f5d92e95b334ac6f44e2ec2f8f8a6e71c4e
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-12-22 11:57:06 +0100

    added text category

--- javascripts/script.js
+++ javascripts/script.js
@@ -17,7 +17,10 @@ function moderation() {
     document.getElementById("info-nav").style.background = "#232323";
     document.getElementById("role").style.display = "none";
     document.getElementById("role-nav").style.background = "#232323";
-    document.getElementById("info").style.display = "none"; document.getElementById("info-nav").style.background = "#232323";
+    document.getElementById("info").style.display = "none";
+    document.getElementById("info-nav").style.background = "#232323";
+    document.getElementById("text").style.display = "none";
+    document.getElementById("text-nav").style.background = "#232323";
 }
 function general() {
     document.getElementById("general").style.display = "block";
@@ -38,7 +41,8 @@ function general() {
     document.getElementById("info-nav").style.background = "#232323";
     document.getElementById("role").style.display = "none";
     document.getElementById("role-nav").style.background = "#232323";
-
+    document.getElementById("text").style.display = "none";
+    document.getElementById("text-nav").style.background = "#232323";
     document.getElementById("info").style.display = "none"; document.getElementById("info-nav").style.background = "#232323";
 }
 function management() {
@@ -60,7 +64,8 @@ function management() {
     document.getElementById("info-nav").style.background = "#232323";
     document.getElementById("role").style.display = "none";
     document.getElementById("role-nav").style.background = "#232323";
-
+    document.getElementById("text").style.display = "none";
+    document.getElementById("text-nav").style.background = "#232323";
     document.getElementById("info").style.display = "none"; document.getElementById("info-nav").style.background = "#232323";
 }
 function api() {
@@ -82,7 +87,8 @@ function api() {
     document.getElementById("info-nav").style.background = "#232323";
     document.getElementById("role").style.display = "none";
     document.getElementById("role-nav").style.background = "#232323";
-
+    document.getElementById("text").style.display = "none";
+    document.getElementById("text-nav").style.background = "#232323";
     document.getElementById("info").style.display = "none"; document.getElementById("info-nav").style.background = "#232323";
 }
 function tag() {
@@ -104,7 +110,8 @@ function tag() {
     document.getElementById("info-nav").style.background = "#232323";
     document.getElementById("role").style.display = "none";
     document.getElementById("role-nav").style.background = "#232323";
-
+    document.getElementById("text").style.display = "none";
+    document.getElementById("text-nav").style.background = "#232323";
     document.getElementById("info").style.display = "none"; document.getElementById("info-nav").style.background = "#232323";
 }
 function chat() {
@@ -126,7 +133,8 @@ function chat() {
     document.getElementById("info-nav").style.background = "#232323";
     document.getElementById("role").style.display = "none";
     document.getElementById("role-nav").style.background = "#232323";
-
+    document.getElementById("text").style.display = "none";
+    document.getElementById("text-nav").style.background = "#232323";
     document.getElementById("info").style.display = "none"; document.getElementById("info-nav").style.background = "#232323";
 }
 function rules() {
@@ -146,11 +154,12 @@ function rules() {
     document.getElementById("rules-nav").style.background = "#7289da";
     document.getElementById("info").style.display = "none";
     document.getElementById("info-nav").style.background = "#232323";
-    document.getElementById("info").style.display = "none"; 
+    document.getElementById("info").style.display = "none";
     document.getElementById("info-nav").style.background = "#232323";
     document.getElementById("role").style.display = "none";
     document.getElementById("role-nav").style.background = "#232323";
-
+    document.getElementById("text").style.display = "none";
+    document.getElementById("text-nav").style.background = "#232323";
 }
 function info() {
     document.getElementById("general").style.display = "none";
@@ -171,7 +180,8 @@ function info() {
     document.getElementById("info").style.display = "block";
     document.getElementById("role").style.display = "none";
     document.getElementById("role-nav").style.background = "#232323";
-
+    document.getElementById("text").style.display = "none";
+    document.getElementById("text-nav").style.background = "#232323";
 }
 function role() {
     document.getElementById("general").style.display = "none";
@@ -190,6 +200,30 @@ function role() {
     document.getElementById("rules-nav").style.background = "#232323";
     document.getElementById("info").style.display = "none";
     document.getElementById("info-nav").style.background = "#232323";
+    document.getElementById("text").style.display = "none";
+    document.getElementById("text-nav").style.background = "#232323";
     document.getElementById("role-nav").style.background = "#7289da";
     document.getElementById("role").style.display = "block";
+}
+function role() {
+    document.getElementById("general").style.display = "none";
+    document.getElementById("management").style.display = "none";
+    document.getElementById("moderation").style.display = "none";
+    document.getElementById("api").style.display = "none";
+    document.getElementById("management-nav").style.background = "#232323";
+    document.getElementById("moderation-nav").style.background = "#232323";
+    document.getElementById("general-nav").style.background = "#232323";
+    document.getElementById("api-nav").style.background = "#232323";
+    document.getElementById("tag").style.display = "none";
+    document.getElementById("tag-nav").style.background = "#232323";
+    document.getElementById("chat").style.display = "none";
+    document.getElementById("chat-nav").style.background = "#232323";
+    document.getElementById("rules").style.display = "none";
+    document.getElementById("rules-nav").style.background = "#232323";
+    document.getElementById("info").style.display = "none";
+    document.getElementById("info-nav").style.background = "#232323";
+    document.getElementById("text").style.display = "block";
+    document.getElementById("role-nav").style.background = "#232323";
+    document.getElementById("text-nav").style.background = "#7289da";
+    document.getElementById("role").style.display = "none";
 }
